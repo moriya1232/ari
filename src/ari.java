3f75b9488caa497061c55c8d693be730248de936
moriya1232 <moriya1232@gmail.com>
2021-11-09 22:30:46 +0200

    Initial commit

---
+++ src/ari.java
@@ -0,0 +1,21 @@
+public class ari {
+
+    public static void main(String[] args) {
+
+        char a = '*';
+        for (int i=0; i<=5; i++) {
+            for(int j=0;j<=5; j++) {
+                if(i==0 || i == 5) {
+                    System.out.print(a);
+                }
+                else if(j==0 || j == 5) {
+                    System.out.print(a);
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
