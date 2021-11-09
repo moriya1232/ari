36011e643ba80127f52b7ff28983f0903d1d9bc7
moriya1232 <moriya1232@gmail.com>
2021-11-09 22:35:59 +0200

    ribua halul

--- src/ari.java
+++ src/ari.java
@@ -3,6 +3,7 @@ public class ari {
     public static void main(String[] args) {
 
         char a = '*';
+
         for (int i=0; i<=5; i++) {
             for(int j=0;j<=5; j++) {
                 if(i==0 || i == 5) {
